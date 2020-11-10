20e080c440570a194b0856bff1ce62c0bd0aee49
Zefanya <Lenovo@Legion5i-ZEFA>
2020-11-10 22:00:08 +0700

    Create MenuAdmin and Fix GUI

--- TubesPBO/src/View/MainScreen.java
+++ TubesPBO/src/View/MainScreen.java
@@ -13,7 +13,8 @@ public class MainScreen {
     public static void main(String[] args) {
 //        new PanelRegister();
 //        new PanelLogin();
-        new MenuUser();
-//        new PanelMenuVendor();
+        new MenuCustomer();
+//        new MenuVendor();
+//        new MenuAdmin();
     }
 }
