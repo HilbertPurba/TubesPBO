55e068443c2630c4ceb4d6152f206d3f7f171172
Gilbert138 <71882722+Gilbert138@users.noreply.github.com>
2020-11-17 14:56:36 +0700

    db baru

--- db_olshop.sql
+++ db_olshop.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2020 at 09:40 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
+-- Waktu pembuatan: 16 Nov 2020 pada 14.53
+-- Versi server: 10.4.14-MariaDB
+-- Versi PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,98 +24,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id_admin` int(3) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `noTelp` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id_admin`, `nama`, `email`, `password`, `noTelp`) VALUES
-(1, 'Kevin', 'kevin@gmail.com', '123456', '08159761575');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
-
-CREATE TABLE `customer` (
-  `id_cust` int(3) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `noTelp` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `customer`
---
-
-INSERT INTO `customer` (`id_cust`, `nama`, `email`, `password`, `noTelp`) VALUES
-(1, 'Zefanya', 'zefa@gmail.com', '123456', '085155340807'),
-(2, 'Gilbert', 'gibe@gmail.com', '123456', '087944568989'),
-(3, 'Hilbert', 'hilbert@gmail.com', '123456', '096899468989');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `keranjang`
+-- Struktur dari tabel `keranjang`
 --
 
 CREATE TABLE `keranjang` (
   `id_keranjang` int(3) NOT NULL,
-  `id_cust` int(3) NOT NULL,
+  `id` int(3) NOT NULL,
   `id_prod` int(3) NOT NULL,
   `jumlah_total` int(10) NOT NULL,
   `harga_total` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `keranjang`
+-- Struktur dari tabel `pengguna`
 --
 
-INSERT INTO `keranjang` (`id_keranjang`, `id_cust`, `id_prod`, `jumlah_total`, `harga_total`) VALUES
-(1, 1, 1, 1, 200000),
-(2, 2, 2, 2, 50000),
-(3, 3, 3, 4, 200000);
+CREATE TABLE `pengguna` (
+  `id` int(3) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `tipeUser` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `product`
+-- Struktur dari tabel `produk`
 --
 
-CREATE TABLE `product` (
+CREATE TABLE `produk` (
   `id_prod` int(3) NOT NULL,
-  `id_vendor` int(3) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `merk` varchar(255) NOT NULL,
+  `id` int(3) NOT NULL,
+  `nama_prod` varchar(255) NOT NULL,
+  `merk_prod` varchar(255) NOT NULL,
   `harga` int(15) NOT NULL,
   `stok` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `product`
---
-
-INSERT INTO `product` (`id_prod`, `id_vendor`, `nama`, `merk`, `harga`, `stok`) VALUES
-(1, 1, 'Mouse Logitech G33', 'Logitech', 200000, 10),
-(2, 2, 'Buku Mengaji Cerdas', 'Mizan', 25000, 20),
-(3, 3, 'Kaos Trendy Yellow', '3Second', 50000, 5);
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaksi`
+-- Struktur dari tabel `transaksi`
 --
 
 CREATE TABLE `transaksi` (
@@ -127,144 +79,89 @@ CREATE TABLE `transaksi` (
   `jenis_pembayaran` varchar(255) NOT NULL,
   `jenis_pengiriman` varchar(255) NOT NULL,
   `kode_promo` varchar(255) NOT NULL,
-  `total_harga` int(15) NOT NULL
+  `total_harga` int(15) NOT NULL,
+  `status_kirim` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `transaksi`
---
-
-INSERT INTO `transaksi` (`id_transaksi`, `id_keranjang`, `nama`, `noTelp`, `alamat`, `jenis_pembayaran`, `jenis_pengiriman`, `kode_promo`, `total_harga`) VALUES
-(1, 1, 'Zefanya', '085155340807', 'Kopo', 'COD', '-', 'DISC10K', 190000),
-(2, 2, 'Gilbert', '087944568989', 'Bitung', 'Transfer Bank', 'JNE', 'DISC10K', 40000),
-(3, 3, 'Hilbert', '096899468989', 'Pasir Kaliki', 'Transfer Bank', 'JNT', '-', 200000);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vendor`
---
-
-CREATE TABLE `vendor` (
-  `id_vendor` int(3) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `noTelp` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `vendor`
---
-
-INSERT INTO `vendor` (`id_vendor`, `nama`, `email`, `password`, `noTelp`) VALUES
-(1, 'Megacomp', 'megacomp@gmail.com', '123456', '085644987946'),
-(2, 'Gramedia Online', 'gramed@gmail.com', '123456', '08659964646'),
-(3, '3Second Online', 'threesecond@gmail.com', '123456', '086566894949');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
-
---
--- Indexes for table `customer`
---
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`id_cust`);
-
---
--- Indexes for table `keranjang`
+-- Indeks untuk tabel `keranjang`
 --
 ALTER TABLE `keranjang`
   ADD PRIMARY KEY (`id_keranjang`),
-  ADD KEY `id_cust` (`id_cust`),
+  ADD KEY `id` (`id`),
   ADD KEY `id_prod` (`id_prod`);
 
 --
--- Indexes for table `product`
+-- Indeks untuk tabel `pengguna`
 --
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`id_prod`),
-  ADD KEY `id_vendor` (`id_vendor`);
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transaksi`
+-- Indeks untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  ADD PRIMARY KEY (`id_prod`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indeks untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
   ADD KEY `id_keranjang` (`id_keranjang`);
 
 --
--- Indexes for table `vendor`
---
-ALTER TABLE `vendor`
-  ADD PRIMARY KEY (`id_vendor`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id_admin` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `customer`
---
-ALTER TABLE `customer`
-  MODIFY `id_cust` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `keranjang`
+-- AUTO_INCREMENT untuk tabel `keranjang`
 --
 ALTER TABLE `keranjang`
-  MODIFY `id_keranjang` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_keranjang` int(3) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `product`
+-- AUTO_INCREMENT untuk tabel `pengguna`
 --
-ALTER TABLE `product`
-  MODIFY `id_prod` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `pengguna`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `transaksi`
+-- AUTO_INCREMENT untuk tabel `produk`
+--
+ALTER TABLE `produk`
+  MODIFY `id_prod` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id_transaksi` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_transaksi` int(3) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `vendor`
---
-ALTER TABLE `vendor`
-  MODIFY `id_vendor` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `keranjang`
+-- Ketidakleluasaan untuk tabel `keranjang`
 --
 ALTER TABLE `keranjang`
-  ADD CONSTRAINT `keranjang_ibfk_1` FOREIGN KEY (`id_cust`) REFERENCES `customer` (`id_cust`),
-  ADD CONSTRAINT `keranjang_ibfk_2` FOREIGN KEY (`id_prod`) REFERENCES `product` (`id_prod`);
+  ADD CONSTRAINT `keranjang_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pengguna` (`id`),
+  ADD CONSTRAINT `keranjang_ibfk_2` FOREIGN KEY (`id_prod`) REFERENCES `produk` (`id_prod`);
 
 --
--- Constraints for table `product`
+-- Ketidakleluasaan untuk tabel `produk`
 --
-ALTER TABLE `product`
-  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`id_vendor`) REFERENCES `vendor` (`id_vendor`);
+ALTER TABLE `produk`
+  ADD CONSTRAINT `produk_ibfk_1` FOREIGN KEY (`id`) REFERENCES `pengguna` (`id`);
 
 --
--- Constraints for table `transaksi`
+-- Ketidakleluasaan untuk tabel `transaksi`
 --
 ALTER TABLE `transaksi`
   ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_keranjang`) REFERENCES `keranjang` (`id_keranjang`);
